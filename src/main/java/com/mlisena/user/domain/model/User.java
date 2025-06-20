bf565d37d87874164d55f3051bcf04877efba1e8
Mauro Lisena <maurolisena@gmail.com>
2025-06-20 08:32:31 -0300

    Resolve imports issue about id annotation

--- src/main/java/com/mlisena/user/domain/model/User.java
+++ src/main/java/com/mlisena/user/domain/model/User.java
@@ -1,8 +1,8 @@
 package com.mlisena.user.domain.model;
 
-import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.index.TextIndexed;
